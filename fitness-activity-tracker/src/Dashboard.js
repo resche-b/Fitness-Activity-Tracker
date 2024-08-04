19e624f11d952a5310c38a7050d8b18187dbda13
rescheb <resche26@live.com>
2024-08-04 13:16:58 -0400

    two secondary graphs

--- fitness-activity-tracker/src/Dashboard.js
+++ fitness-activity-tracker/src/Dashboard.js
@@ -2,27 +2,37 @@ import React from "react";
 import WaterTracker from "./WaterTracker";
 import SleepTracker from "./SleepTracker";
 import CaloriesTracker from "./CaloriesTracker";
+import StepTracker from "./StepTracker";
 import "./Dashboard.css";
+import WeightTracker from "./WeightTracker";
 
 const Dashboard = () => {
   return (
-    <div className="trackers">
-      <div className="specific-tracker-main">
-        <div className="specific-tracker">
-          <WaterTracker />
+    <div>
+      <div className="trackers">
+        <div className="specific-tracker-main">
+          <div className="specific-tracker">
+            <WaterTracker />
+          </div>
         </div>
-       
-      </div>
-      <div className="specific-tracker-main">
-        <div className="specific-tracker">
-          <SleepTracker />
+        <div className="specific-tracker-main">
+          <div className="specific-tracker">
+            <SleepTracker />
+          </div>
         </div>
-        
+        <div className="calories-tracker-main">
+          <div className="calories-tracker">
+            <CaloriesTracker />
+          </div>
+        </div>
       </div>
-      <div className="calories-tracker-main">
-      <div className="calories-tracker">
-        <CaloriesTracker/>
-      </div>
+      <div className="secondary-row">
+        <div className="step-tracker-main">
+          <StepTracker />
+        </div>
+        <div className="weight-tracker-main">
+          <WeightTracker/>
+        </div>
       </div>
     </div>
   );
---
+++ fitness-activity-tracker/src/WeightTracker.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from "react";
+import Chart from "chart.js/auto";
+import { useState } from "react";
+
+const WeightTracker = () => {
+  const chartRef = useRef(null);
+  const chartInstance = useRef(null);
+  const [weight, setWeight] = useState(5);
+
+  useEffect(() => {
+    if (chartInstance.current) {
+      chartInstance.current.destroy();
+    }
+
+    const myChartRef = chartRef.current.getContext("2d");
+    chartInstance.current = new Chart(myChartRef, {
+      type: "line",
+      data: {
+        labels: ['test1','test2'],
+        datasets: [
+          {
+            label: "My First Dataset",
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: "rgb(75, 192, 192)",
+            tension: 0.1,
+          },
+        ],
+      },
+    });
+
+    return () => {
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <canvas ref={chartRef} />
+    </div>
+  );
+};
+
+export default WeightTracker;
